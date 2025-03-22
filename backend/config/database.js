e76f7115d9e2bc2979f8330ea62c8f3bfec81145
Rene-Marcel Lehner <rene.lehner@tu-dortmund.de>
2025-03-22 09:22:13 +0100

    Update database configuration and container names; add backup and import scripts

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
-        host: env('POSTGRES_HOST', 'strapiDB'), // Container name for the PostgreSQL service
+        host: env('POSTGRES_HOST', 'strapiDB_deeprecall'), // Container name for the PostgreSQL service
         port: env.int('POSTGRES_PORT', 5432),
         database: env('POSTGRES_NAME', 'main'),
         user: env('POSTGRES_USER', 'renlephy'),
